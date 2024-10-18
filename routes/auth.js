6d6e30123e94d660b23f80e7ca5edd46ca7487f6
Srujan M Padmashali <88569710+srujanmp@users.noreply.github.com>
2024-10-18 08:35:44 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 require('dotenv').config();
 
 // Hardcoded admin emails
-const allowedAdminEmails = ['srujanmpadmashali@gmail.com', 'mithunmallya97@gmail.com','sumukharao@gmail.com','shreyasrujan1521@gmail.com'];
+const allowedAdminEmails = ['srujanmpadmashali@gmail.com', 'mithunmallya97@gmail.com','sumukharaoh05@gmail.com','shreyasrujan1521@gmail.com'];
 
 // Allowed email domains
 const allowedDomains = ['nmamit.in'];
